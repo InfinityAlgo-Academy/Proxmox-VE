1c61428d4f99e1ac989408c664d5ccfaae47220b
oz <oz@example.com>
2026-04-24 16:08:38 +0100

    Update theme: Obsidian dark purple colors

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Proxmox VE": "https://github.com/InfinityAlgo-Academy/Proxmox-VE",
     },
   }),
 }
